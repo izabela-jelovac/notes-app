fc8978da69a91ba3e7f0cfd1e2ff91cd312d01d8
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-04-16 23:56:38 +0200

    add button to create new notes

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -3,6 +3,9 @@ import Notes from "./Notes";
 function App() {
   return (
     <div className="App">
+      <button className="button-create-new-note" type="button">
+        +create new note
+      </button>
       <Notes></Notes>
     </div>
   );
