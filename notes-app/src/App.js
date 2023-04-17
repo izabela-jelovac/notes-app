1458c8f653734fcb829ae86ef3fa24bb4d6413b5
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-04-17 16:05:45 +0200

    log hardcoded text into console on click of the button

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import Notes from "./Notes";
 function App() {
+  const handleClick = (event) => {
+    console.log("Click");
+  };
   return (
     <div className="App">
-      <button className="button-create-new-note" type="button">
+      <button
+        className="button-create-new-note"
+        type="button"
+        onClick={handleClick}
+      >
         +create new note
       </button>
       <Notes></Notes>
